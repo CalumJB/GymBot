6726feee9fded678b94666795fee666792d41178
calum boustead <calumboustead@calums-mbp.mynet>
2020-11-20 09:03:00 +0000

    Added selenium hub and single node, deployed using kubernetes yamls

---
+++ gym-timetable/src/main/java/com/boustead/gymtimetable/GymTimetableApplication.java
@@ -0,0 +1,13 @@
+package com.boustead.gymtimetable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymTimetableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymTimetableApplication.class, args);
+	}
+
+}
---
+++ gym-timetable/src/test/java/com/boustead/gymtimetable/GymTimetableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boustead.gymtimetable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymTimetableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
